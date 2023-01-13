6d57077215b9320928f0eb49cba39ec5504ec951
Xwilarg <xwilarg@protonmail.com>
2023-01-13 14:33:03 +0100

    Add ability to run from current folder

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+$loader = new FilesystemLoader("templates");
+$twig = new Environment($loader);
+
+# Display page
+echo $twig->render("demo.html.twig");
---
+++ src/init.js
@@ -0,0 +1,3 @@
+window.onload = function () {
+    rpg_init();
+};
